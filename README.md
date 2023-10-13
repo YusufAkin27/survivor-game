f7319f6e2be7d49644ae1c268cda524f3e9a3610
yusuf akin <136736997+YusufAkin27@users.noreply.github.com>
2023-10-13 14:34:29 +0300

    selam

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # survivor-game
+* dsada
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("herkese selam");
+
+        System.out.println("fsdhıf");
     }
 }
