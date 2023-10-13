ecdeec31175f32dd2f0179a95e9ddefb7d94db7f
lenovo <esraesr88@gmail.com>
2023-10-13 15:01:57 +0300

    hghj

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("herkese selam");
-        System.out.printf("gug");
+        System.out.println("fdshıohfd");
+
+        System.out.println("fsdhıf");
+        System.out.printf("jjasj");
     }
 }
