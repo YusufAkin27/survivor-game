d30630a68bbdc59970c6f5d8d32283b19e970db2
yusuf akin <136736997+YusufAkin27@users.noreply.github.com>
2023-10-13 14:55:47 +0300

    selam

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("herkese selam");
+        System.out.println("fdshıohfd");
 
         System.out.println("fsdhıf");
     }
