6f5c90427e6be08a47f71202e993e9cbc90848ac
yusuf akin <136736997+YusufAkin27@users.noreply.github.com>
2023-10-13 15:25:59 +0300

    hghj

--- src/Character.java
+++ src/Character.java
@@ -2,4 +2,37 @@ public class Character {
     private String name;
     private int healty;
     private  int damage;
+    private  int money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealty() {
+        return healty;
+    }
+
+    public void setHealty(int healty) {
+        this.healty = healty;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
