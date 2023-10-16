804b433f5940b7f0bb3f6966975134ea689d2e5f
yusuf akin <136736997+YusufAkin27@users.noreply.github.com>
2023-10-16 11:46:17 +0300

    mağaza tarafı güncellendi

---
+++ src/Story.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Story {
+    Scanner scanner = new Scanner(System.in);
+    Menu menu = new Menu();
+
+    public void storyMenu(Character character) {
+        System.out.println("zırhları listelemek için --> 1");
+        System.out.println("silahları listelemek için --> 2");
+        System.out.println("yetenekleri listelemek için --> 3");
+        System.out.println("Ana Menüye dönmek için --> 4");
+        int value = scanner.nextInt();
+        satınAl(character, value);
+    }
+
+    private Character satınAl(Character character, int value) {
+        switch (value) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                menu.mainMenu();
+            default:
+                System.out.println("hatalı tuşlama yaptınız");
+                storyMenu(character);
+                break;
+        }
+    }
+
+}
